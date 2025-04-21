ca8cc25b29082682bbb455b0e39d1835d290706a
Guna2003 <94002787+Guna2003@users.noreply.github.com>
2025-04-22 02:10:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,23 @@
 import re
+import spacy
+import subprocess
 
-def detect_entities(text):
+def ensure_spacy_model():
+    try:
+        spacy.load("en_core_web_sm")
+    except OSError:
+        print("Downloading spaCy model...")
+        subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+
+ensure_spacy_model()
+nlp = spacy.load("en_core_web_sm")
+
+def detect_regex_entities(text):
     entities = []
 
     patterns = {
         'email': r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b',
         'phone': r'\b\d{10,12}\b',
-        'name': r'\b(John Doe|Gunasekhar|Vaishnavi)\b',
         'aadhar': r'\b\d{4}\s\d{4}\s\d{4}\b',
         'expiry': r'\b(0[1-9]|1[0-2])\/?([0-9]{2})\b',
         'cvv': r'\b[0-9]{3}\b',
@@ -23,10 +34,38 @@ def detect_entities(text):
 
     return entities
 
+def detect_name_entities(text):
+    entities = []
+    doc = nlp(text)
+    for ent in doc.ents:
+        if ent.label_ == "PERSON":
+            entities.append({
+                'start': ent.start_char,
+                'end': ent.end_char,
+                'entity': ent.text,
+                'label': 'name'
+            })
+    return entities
+
 def mask_pii(text):
-    entities = detect_entities(text)
-    entities_sorted = sorted(entities, key=lambda x: x['start'], reverse=True)
+    regex_entities = detect_regex_entities(text)
+    name_entities = detect_name_entities(text)
+    all_entities = regex_entities + name_entities
+
+   
+    entities_sorted = sorted(all_entities, key=lambda x: x['start'])
+
+    masked_text = ""
+    current_pos = 0
     for ent in entities_sorted:
+        start, end = ent['start'], ent['end']
         label = ent['label'].upper()
-        text = text[:ent['start']] + f"<{label} MASKED>" + text[ent['end']:]
-    return text, entities
+
+        if start >= current_pos:  
+            masked_text += text[current_pos:start] + f"<{label} MASKED>"
+            current_pos = end
+
+    
+    masked_text += text[current_pos:]
+
+    return masked_text, all_entities
